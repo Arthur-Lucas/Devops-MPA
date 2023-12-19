4762ca90a0c1a5ee4c8558bae508982d867403d3
Matis Dene <matis.dene@edu.devinci.fr>
2023-12-19 13:49:55 +0100

    auth added

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,15 @@
 export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "devopsmpa274fb0d7f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "storage": {
     "users": {
       "Arn": "string",
