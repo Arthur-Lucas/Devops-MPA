d6a53542fe0712db0bc0403d3b82cb055c6ac43a
Arthur <arthurlucas92@orange.fr>
2023-12-21 10:57:20 +0100

    dev api see user details in progress

--- amplify/backend/function/devopsmpa2getdetailsuser/src/index.js
+++ amplify/backend/function/devopsmpa2getdetailsuser/src/index.js
@@ -1,17 +1,61 @@
-
-
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
+const AWS = require("aws-sdk");
+
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+  //   return {
+  //     statusCode: 200,
+  //     //  Uncomment below to enable CORS requests
+  //     //  headers: {
+  //     //      "Access-Control-Allow-Origin": "*",
+  //     //      "Access-Control-Allow-Headers": "*"
+  //     //  },
+
+  //     body: JSON.stringify("Hello from Lambda!"),
+  //   };
+
+  const userId = event.pathParameters.id;
+
+  // Configuration de l'accès à DynamoDB
+  AWS.config.update({
+    region: "eu-west-3", // Remplacez par la région de votre DynamoDB
+  });
+
+  const documentClient = new AWS.DynamoDB.DocumentClient();
+
+  // Paramètres de requête DynamoDB
+  const params = {
+    TableName: "users",
+    Key: {
+      userId: userId,
+    },
+  };
+
+  try {
+    const data = await documentClient.get(params).promise();
+
+    if (data.Item) {
+      return {
         statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify(data.Item),
+      };
+    } else {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "Utilisateur non trouvé" }),
+      };
+    }
+  } catch (error) {
+    console.error(
+      "Erreur lors de la récupération des informations de l'utilisateur",
+      error
+    );
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Erreur serveur" }),
     };
+  }
 };
--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
-import React from 'react';
-import { Amplify } from 'aws-amplify';
+import React, { useEffect, useState } from "react";
+import { Amplify } from "aws-amplify";
+import { get } from "aws-amplify/api";
 
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-import awsExports from './aws-exports';
+import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
 export default function App() {
- 
+  const [userData, setUserData] = useState(null);
+  const [user, setUser] = useState(null);
+
+  // useEffect(() => {
+  //   // Fonction pour charger les données de l'utilisateur
+  //   const loadUserData = async () => {
+  //     try {
+  //       const apiResponse = await API.get(
+  //         "apiGetDetailsUser",
+  //         `/user/${user.username}`
+  //       );
+  //       setUserData(apiResponse);
+  //     } catch (error) {
+  //       console.error(
+  //         "Erreur lors de la récupération des données de l'utilisateur",
+  //         error
+  //       );
+  //     }
+  //   };
+
+  //   // Charger les données de l'utilisateur uniquement s'il est authentifié
+  //   if (user) {
+  //     loadUserData();
+  //   }
+  // }, [user]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!user) {
+        // Si l'utilisateur n'est pas défini, ne rien faire
+        return;
+      }
+
+      try {
+        // console.log(user);
+        const apiResponse = await get({
+          apiName: "apiGetDetailsUser",
+          path: `/user/${user.username}`,
+        });
+        const response = await apiResponse.response;
+        console.log(response);
+      } catch (error) {
+        console.error(
+          "Erreur lors de la récupération des données de l'utilisateur",
+          error
+        );
+      }
+    };
+
+    fetchData();
+  }, [user]);
+
   return (
-    <Authenticator initialState="signUp" signUpAttributes={[ 'family_name', 'name',]}>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
+    <Authenticator initialState="" signUpAttributes={["family_name", "name"]}>
+      {({ signOut, user }) => {
+        setUser(user);
+        return (
+          <main>
+            <h1>Hello {user.username}</h1>
+            {/* Affichez les données de l'utilisateur */}
+            {userData && <p>User Data: {JSON.stringify(userData)}</p>}
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        );
+      }}
     </Authenticator>
   );
 }
